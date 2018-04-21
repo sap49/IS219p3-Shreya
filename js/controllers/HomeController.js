1981b6594f4ede82d5fc6edf01706c4b8fa32997
Shreya Patel <sap87>
2018-04-21 19:15:24 -0400

    Fix the syntax for the data-bindings to display a movie’s information using Angular code  part 5

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -20,6 +20,76 @@ app.controller('HomeController', ['$scope', function($scope) {
 	*/
 	$scope.movies = [
     { 
+      title:	'The Shawshank Redemption', 
+      iscore:	9.3,
+      rating:	'R',
+      runtime:	142,
+      released:	new Date('1994', '10', '14'),
+      country:	'USA',
+      posters:	['img/shawshank.jpg','img/shawshank2.jpg','img/shawshank3.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0111161',
+      website:	'https://www.facebook.com/ShawshankRedemptionFilm',
+      likes:	1021,
+      dislikes:	122,
+      posterindex: 0
+	},
+    { 
+      title:	'The Godfather', 
+      iscore:	9.2,
+      rating:	'R',
+      runtime:	175,
+      released:	new Date('1972', '03', '24'),
+      country:	'USA',
+      posters:	['img/godfather.jpg','img/godfather_2.jpg','img/godfather_3.jpg'],
+      imdb:		'[http://www.imdb.com/title/tt0068646',
+      website:	'https://www.facebook.com/thegodfather',
+      likes:	928,
+      dislikes:	109,
+      posterindex: 0
+	},
+    { 
+      title:	'The Godfather: Part II', 
+      iscore:	9.0,
+      rating:	'R',
+      runtime:	202,
+      released:	new Date('1974', '12', '20'),
+      country:	'USA',
+      posters:	['img/godfather2.jpg','img/godfather2_2.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0071562',
+      website:	'https://www.facebook.com/thegodfather',
+      likes:	855,
+      dislikes:	99,
+      posterindex: 0
+	},
+    { 
+      title:	'The Dark Knight', 
+      iscore:	8.9,
+      rating:	'PG-13',
+      runtime:	152,
+      released:	new Date('2008', '07', '18'),
+      country:	'USA',
+      posters:	['img/darkknight.jpg','img/darkknight2.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0468569',
+      website:	'http://www.42entertainment.com/work/whysoserious',
+      likes:	828,
+      dislikes:	127,
+      posterindex: 0
+	},
+    { 
+      title:	'Schindler\'s List', 
+      iscore:	8.9,
+      rating:	'R',
+      runtime:	195,
+      released:	new Date('1993', '11', '30'),
+      country:	'USA',
+      posters:	['img/schindlerslist.jpg'],
+      imdb:		'http://www.imdb.com/title/tt0108052',
+      website:	'https://www.uphe.com/schindlers-list',
+      likes:	812,
+      dislikes:	101,
+      posterindex: 0
+	},
+	{
       title:	'Deep Impact', 
       iscore:	6.1,
       rating:	'PG-13',
@@ -33,35 +103,7 @@ app.controller('HomeController', ['$scope', function($scope) {
       dislikes:	122,
       posterindex: 0
 	},
-    { 
-      title:	'Harry Potter Goblet of Fire ', 
-      iscore:	7.7,
-      rating:	'PG-13',
-      runtime:	166,
-      released:	new Date('2005'),
-      country:	'USA',
-      posters:	['img/HP Goblet of Fire.jpg'],
-      imdb:		'http://www.imdb.com/title/tt0330373/?ref_=nv_sr_3',
-      website:	'https://www.facebook.com/harrypottermovie/',
-      likes:	1566,
-      dislikes:	5550,
-      posterindex: 0
-	},
-    { 
-      title:	'Captain America: Civil War', 
-      iscore:	9.0,
-      rating:	'R',
-      runtime:	202,
-      released:	new Date(''),
-      country:	'USA',
-      posters:	['img/CA Civil War.jpg'],
-      imdb:		'http://www.imdb.com/title/tt0071562',
-      website:	'https://www.facebook.com/thegodfather',
-      likes:	855,
-      dislikes:	99,
-      posterindex: 0
-	},
-    { 
+      {
       title:	'Speed', 
       iscore:	8.9,
       rating:	'R',
@@ -75,7 +117,7 @@ app.controller('HomeController', ['$scope', function($scope) {
       dislikes:	127,
       posterindex: 0
 	},
-    { 
+      { 
       title:	'Titanic', 
       iscore:	7.8,
       rating:	'PG-13',
@@ -93,8 +135,6 @@ app.controller('HomeController', ['$scope', function($scope) {
 	
   ];
 	
-	
-
 	
 	/* ADD VARIABLES FOR STEP 3 HERE */
 	
